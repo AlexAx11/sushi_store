34b3833ba9b0f4d69c06178de29299f7b6bd1fd0
AlexAx11 <alexaxyanov@gmail.com>
2022-04-06 20:32:21 +0300

    Update firebase-config.ts

--- src/config/firebase-config.ts
+++ src/config/firebase-config.ts
@@ -1,7 +1,13 @@
 import firebase from 'firebase'
 import 'firebase/storage'
 export const app = firebase.initializeApp({
-   
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 });
 
 export  {
